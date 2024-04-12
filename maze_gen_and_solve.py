1b8bd0c7f4932c1360118a145ee71f30c7e7a191
Evan Hekman <ehekman@umich.edu>
2024-04-12 18:37:03 -0400

    ready to generate

--- maze_gen_and_solve.py
+++ maze_gen_and_solve.py
@@ -8,14 +8,93 @@ import os
 sys.setrecursionlimit(10000)
 maze_number = 0
 
+# def generate_maze(width, height):
+#     # Initialize the maze grid as a solid block (walls everywhere)
+#     maze = [[0] * (2 * width + 1) for _ in range(2 * height + 1)]
+
+#     # Carve out the internal area for the maze paths
+#     for x in range(width):
+#         for y in range(height):
+#             maze[2 * y + 1][2 * x + 1] = 1
+
+#     stack = [(0, 0)]
+#     visited = {(0, 0)}
+
+#     while stack:
+#         x, y = stack[-1]
+#         neighbors = []
+#         for nx, ny in [(x - 1, y), (x + 1, y), (x, y - 1), (x, y + 1)]:
+#             if 0 <= nx < width and 0 <= ny < height and (nx, ny) not in visited:
+#                 neighbors.append((nx, ny))
+
+#         if neighbors:
+#             nx, ny = random.choice(neighbors)
+#             # Remove the wall between the current cell and the chosen cell
+#             maze[2 * y + 1 + (ny - y)][2 * x + 1 + (nx - x)] = 0
+#             stack.append((nx, ny))
+#             visited.add((nx, ny))
+#         else:
+#             stack.pop()
+
+#     # Randomize the start and end points on the boundary
+#     # Ensure start and end are on different boundaries if chosen from top/bottom or left/right
+#     sides = ['top', 'right', 'bottom', 'left']
+#     anakin_skywalker = random.randint(0, 3)
+#     start_side = sides[anakin_skywalker]  # Choose two distinct sides
+#     end_side = sides[(anakin_skywalker + 2) % 4]
+
+#     # Start point
+#     if start_side == 'top':
+#         start = (-1, -1)
+#         while start[0] == -1 or maze[1][start[1]] == 0:
+#             start = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+#     elif start_side == 'bottom':
+#         start = (-1, -1)
+#         while start[0] == -1 or maze[height - 2][start[1]] == 0:
+#             start = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+#     elif start_side == 'left':
+#         start = (-1, -1)
+#         while start[0] == -1 or maze[start[0]][1] == 0:
+#             start = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+#     else:  # 'right'
+#         start = (-1, -1)
+#         while start[0] == -1 or maze[start[0]][width - 2] == 0:
+#             start = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+
+#     # End point
+#     if end_side == 'top':
+#         end = (-1, -1)
+#         while end[0] == -1 or maze[1][end[1]] == 0:
+#             end = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+#     elif end_side == 'bottom':
+#         end = (-1, -1)
+#         while end[0] == -1 or maze[height - 2][end[1]] == 0:
+#             end = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+#     elif end_side == 'left':
+#         end = (-1, -1)
+#         while end[0] == -1 or maze[end[0]][1] == 0:
+#             end = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+#     else:  # 'right'
+#         end = (-1, -1)
+#         while end[0] == -1 or maze[end[0]][width - 2] == 0:
+#             end = (random.choice(range(1, height - 1)), random.choice(range(1, width - 1)))
+
+#     # Open the start and end points
+#     maze[start[0]][start[1]] = 1
+#     maze[end[0]][end[1]] = 1
+
+#     return maze, start, end
+
+import random
+
 def generate_maze(width, height):
     # Initialize the maze grid as a solid block (walls everywhere)
-    maze = [[1] * (2 * width + 1) for _ in range(2 * height + 1)]
+    maze = [[0] * (2 * width + 1) for _ in range(2 * height + 1)]
 
     # Carve out the internal area for the maze paths
     for x in range(width):
         for y in range(height):
-            maze[2 * y + 1][2 * x + 1] = 0
+            maze[2 * y + 1][2 * x + 1] = 1
 
     stack = [(0, 0)]
     visited = {(0, 0)}
@@ -30,7 +109,7 @@ def generate_maze(width, height):
         if neighbors:
             nx, ny = random.choice(neighbors)
             # Remove the wall between the current cell and the chosen cell
-            maze[2 * y + 1 + (ny - y)][2 * x + 1 + (nx - x)] = 0
+            maze[2 * y + 1 + (ny - y)][2 * x + 1 + (nx - x)] = 1
             stack.append((nx, ny))
             visited.add((nx, ny))
         else:
@@ -39,49 +118,33 @@ def generate_maze(width, height):
     # Randomize the start and end points on the boundary
     # Ensure start and end are on different boundaries if chosen from top/bottom or left/right
     sides = ['top', 'right', 'bottom', 'left']
-    anakin_skywalker = random.randint(0, 3)
-    start_side = sides[anakin_skywalker]  # Choose two distinct sides
-    end_side = sides[(anakin_skywalker + 2) % 4]
+    start_side = random.choice(sides)
+    end_side = sides[(sides.index(start_side) + 2) % 4]
+    # end_side = random.choice([side for side in sides if side != start_side])
 
     # Start point
     if start_side == 'top':
-        start = (-1, -1)
-        while start[0] == -1 or maze[0][start[1]+1] == 0:
-            start = (0, random.choice(range(1, 2 * width - 2, 2)))
+        start = (1, random.randint(1, width * 2 - 1))
     elif start_side == 'bottom':
-        start = (-1, -1)
-        while start[0] == -1 or maze[2 * height][start[1]-1] == 0:
-            start = (2 * height, random.choice(range(1, 2 * width - 2, 2)))
+        start = (height * 2 - 1, random.randint(1, width * 2 - 1))
     elif start_side == 'left':
-        start = (-1, -1)
-        while start[0] == -1 or maze[start[0]+1][0] == 0:
-            start = (random.choice(range(1, 2 * height - 2, 2)), 0)
+        start = (random.randint(1, height * 2 - 1), 1)
     else:  # 'right'
-        start = (-1, -1)
-        while start[0] == -1 or maze[start[0]-1][2 * width] == 0:
-            start = (random.choice(range(1, 2 * height - 2, 2)), 2 * width)
+        start = (random.randint(1, height * 2 - 1), width * 2 - 1)
 
     # End point
     if end_side == 'top':
-        end = (-1, -1)
-        while end[0] == -1 or maze[0][end[1]+1] == 0:
-            end = (0, random.choice(range(1, 2 * width - 2, 2)))
+        end = (1, random.randint(1, width * 2 - 1))
     elif end_side == 'bottom':
-        end = (-1, -1)
-        while end[0] == -1 or maze[2 * height][end[1]-1] == 0:
-            end = (2 * height, random.choice(range(1, 2 * width - 2, 2)))
+        end = (height * 2 - 1, random.randint(1, width * 2 - 1))
     elif end_side == 'left':
-        end = (-1, -1)
-        while end[0] == -1 or maze[end[0]+1][0] == 0:
-            end = (random.choice(range(1, 2 * height - 2, 2)), 0)
+        end = (random.randint(1, height * 2 - 1), 1)
     else:  # 'right'
-        end = (-1, -1)
-        while end[0] == -1 or maze[end[0]-1][2 * width] == 0:
-            end = (random.choice(range(1, 2 * height - 2, 2)), 2 * width)
+        end = (random.randint(1, height * 2 - 1), width * 2 - 1)
 
     # Open the start and end points
-    maze[start[0]][start[1]] = 0
-    maze[end[0]][end[1]] = 0
+    maze[start[0]][start[1]] = 1
+    maze[end[0]][end[1]] = 1
 
     return maze, start, end
 
@@ -93,7 +156,7 @@ def visualize_maze(maze, start, end):
 
     plt.figure(figsize=(10, 5))
     # Custom color map: walls in black, path in white, start in green, end in red
-    cmap = ListedColormap(['white', 'black', 'green', 'red'])
+    cmap = ListedColormap(['black', 'white', 'green', 'red'])
     bounds = [0,1,2,3,4]
     norm = BoundaryNorm(bounds, cmap.N)
 
@@ -115,7 +178,7 @@ def find_path_dfs(maze, start, end):
         directions = [(0, 1), (1, 0), (0, -1), (-1, 0)]
         for dx, dy in directions:
             nx, ny = x + dx, y + dy
-            if 0 <= nx < height and 0 <= ny < width and not visited[nx][ny] and maze[nx][ny] != 1:
+            if 0 <= nx < height and 0 <= ny < width and not visited[nx][ny] and maze[nx][ny] != 0:
                 visited[nx][ny] = True
                 if dfs(nx, ny):
                     path.append((x, y))
@@ -152,45 +215,54 @@ def visualize_maze_with_path(maze, start, end, path):
     maze_np[end[0]][end[1]] = 3
 
     plt.figure(figsize=(10, 5))
-    cmap = ListedColormap(['white', 'black', 'green', 'red', 'blue'])
+    cmap = ListedColormap(['black', 'white', 'green', 'red', 'blue'])
     bounds = [0,1,2,3,4,5]
     norm = BoundaryNorm(bounds, cmap.N)
 
     plt.imshow(maze_np, cmap=cmap, norm=norm)
     plt.xticks([]), plt.yticks([])
     plt.show()
+
+def construct_datapoint(maze, dir, reward=0.1):
+    mazevals = []
+    data = ""
+    for r in maze:
+        for c in r:
+            mazevals.append(c)
+    data += str(mazevals)
+    data += '\n'
+    data += dir
+    data += '\n'
+    data += str(reward)
+    data += '\n'
+    return data
     
-def save_maze_and_solutions(maze, path):
+def export_dataset(folder, dataset, n):
+    with open(f"{folder}/maze{n}.txt", 'w') as file:
+        for i in range(len(dataset)):
+            file.write(str(dataset[i]))
+
+def generate_dataset(maze, path):
+    print(path)
+    dataset = []
     maze[path[0][0]][path[0][1]] = 3
     maze[path[len(path) - 1][0]][path[len(path) - 1][1]] = 5
-    # path = path[::-1]
-    for i in range(1, len(path) - 2):
-        if random.randint(0, 100) == 36:
+    for i in range(1, len(path)):
+        if i != len(path) - 1:
             path_ = (path[i], path[i + 1])
-            # Save the maze grid
-            with open(f"large_mazes/large_mazes{maze_number}_iteration_{i}.txt", "w") as file:
-                og0 = maze[path_[0][0]][path_[0][1]]
-                maze[path_[0][0]][path_[0][1]] = 4
-                for row in maze:
-                    file.write(''.join(str(cell) for cell in row) + "\n")
-                file.write("\n")
+            foobar = maze[path_[0][0]][path_[0][1]]
+            maze[path_[0][0]][path_[0][1]] = 4
+            maze[path_[0][0]][path_[0][1]] = foobar
+        dx = path[i][0] - path[i-1][0]
+        dy = path[i][1] - path[i-1][1]
+        if dx == 1: dir = "D"
+        elif dx == -1: dir = "U"
+        if dy == 1: dir = "R"
+        elif dy == -1: dir = "L"
 
-                maze[path_[0][0]][path_[0][1]] = og0
-
-                # Calculate moves from the path_
-                moves = []
-                for i in range(1, len(path_)):
-                    dx = path_[i][0] - path_[i-1][0]
-                    dy = path_[i][1] - path_[i-1][1]
-                    # print(dx, dy)
-                    if dx == 1: file.write("D\n")
-                    elif dx == -1: file.write("U\n")
-                    if dy == 1: file.write("R\n")
-                    elif dy == -1: file.write("L\n")
-                
-                # Save the solution moves
-                # file.write("".join(moves) + "\n")
-            break
+        data = construct_datapoint(maze, dir, reward=(i / len(path)))
+        dataset.append(data)
+    return dataset
 
 from queue import PriorityQueue
 
@@ -214,14 +286,13 @@ def a_star_search(maze, start, end):
 
         for dx, dy in [(0, 1), (1, 0), (0, -1), (-1, 0)]:
             next = (current[0] + dx, current[1] + dy)
-            if 0 <= next[0] < len(maze) and 0 <= next[1] < len(maze[0]) and maze[next[0]][next[1]] == 0:
+            if 0 <= next[0] < len(maze) and 0 <= next[1] < len(maze[0]) and maze[next[0]][next[1]] == 1:
                 new_cost = cost_so_far[current] + 1
                 if next not in cost_so_far or new_cost < cost_so_far[next]:
                     cost_so_far[next] = new_cost
                     priority = new_cost + heuristic(end, next)
                     queue.put((priority, next))
                     came_from[next] = current
-
     # Reconstruct path
     current = end
     path = []
@@ -248,12 +319,12 @@ def adjust_path_for_maze(path):
 #     if "small" not in filename and "medium" not in filename and "large" not in filename:
 #         os.remove(f"mazes/{filename}")
 
-input("are you sure you want to pollute your dataset? ctrl-c to stop")
-for j in range(4):
-    width, height = 50, 50
+# if __name__=="main":
+# for j in range(4):
+for n in range(1000):
+    width, height = 5, 5
     maze, start, end = generate_maze(width, height)
-    maze_number = j
     path = a_star_search(maze, (start[0], start[1]), (end[0], end[1]))
-    # save_maze_and_solutions(maze, path)
-    visualize_maze_with_path(maze, start, end, path)
-
+    dataset = generate_dataset(maze, path)
+    export_dataset("dataset_of_1000_small", dataset, n)
+    # visualize_maze_with_path(maze, start, end, path)
